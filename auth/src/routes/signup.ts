fff3f35b1768a5be61915a09fddaf4a8a67462ec
shubhamsaxena <shubhamsaxena350@gmail.com>
2022-03-28 17:57:54 +0530

    NextJS, Node, Jest,  Microservices, Docker, Kubernetes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,10 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
-
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
-//import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError, validateRequest } from '@shubstickets/common';
 
 const router = express.Router();
 
