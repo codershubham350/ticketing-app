7bafaf077d37400245e9564ab0dbdeb35aec14f8
shubhamsaxena <shubhamsaxena350@gmail.com>
2022-04-03 23:27:26 +0530

    NextJs, Microservices, Node, Jest, Docker, Kubernetes

---
+++ tickets/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@shubstickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+
+const app = express();
+app.set('trust proxy', true); // traffic is proxied through Ingress NGINX
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'assdasda';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payload. {id, email, iat (issued at time)}
+  const payload = {
+    id: '1jgg34jgjg56',
+    email: 'test@test.com',
+  };
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  // Build session Object. {jwt: MY_JWT}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with encoded data
+  return [`session=${base64}`];
+};
